fbd6669b4f97e4df35a3544a7218a28013d3aaa0
Ryan <rdturner40@gmail.com>
2021-06-09 16:01:25 -0400

    wip

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import React, {FC, ReactElement} from 'react';
+  import {useStyles} from './styles';
+  import {FOOTER_TEXT} from '../../utils/constants'
+
+  interface FooterProps {
+    // Props go here
+  }
+  
+  const Footer: FC<FooterProps> = (props): ReactElement => {
+    const classes = useStyles()
+    return (
+      <div className={classes.footerWrapper}>
+        {FOOTER_TEXT}
+      </div>
+      )
+  }
+  
+  export default Footer
+  
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,24 @@
 import React, {FC, ReactElement} from 'react';
+import clsx from 'clsx'
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  IconButton,
+  Tooltip,
+} from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import Brightness7Icon from '@material-ui/icons/Brightness7';
+import Brightness3Icon from '@material-ui/icons/Brightness3';
+import LogoLight from '../LogoLight/index'
 import {useStyles} from './styles';
 
   interface HeaderProps {
-    open: boolean;
-    handleMenuOpen: ()=>void;
-    toggleTheme: ()=>void;
-    useDefaultTheme: boolean;
+    open?: boolean;
+    handleMenuOpen?: ()=>void;
+    toggleTheme?: ()=>void;
+    useDefaultTheme?: boolean;
   }
   
   const Header: FC<HeaderProps> = ({
@@ -16,8 +29,43 @@ import {useStyles} from './styles';
   }): ReactElement => {
     const classes = useStyles()
     return (
-      <div>
-      </div>
+      <>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        elevation={0}
+        className={classes.appBar}
+        >
+          <Toolbar className={classes.toolbar}>
+            <div className={classes.logo}>
+                <LogoLight width='80px' height='50px'/>
+            </div>
+            {/* <IconButton onClick={toggleTheme}>
+              {useDefaultTheme ? (
+                <Tooltip title='Switch to dark mode' placement='bottom'>
+                  <Brightness3Icon />
+                </Tooltip>
+              ): (
+                <Tooltip title='Switch to light mode' placement='bottom'>
+                  <Brightness7Icon />
+                </Tooltip>
+              )}
+            </IconButton> */}
+            <IconButton
+                  color="inherit"
+                  aria-label="open menu"
+                  onClick={handleMenuOpen}
+                  edge="start"
+                  className={clsx(classes.menuButton, {
+                    [classes.hide]: open,
+                  })}
+                  size="small"
+                  >
+                    <MenuIcon />
+                </IconButton>
+          </Toolbar>
+        </AppBar>
+      </>
       )
   }
   
